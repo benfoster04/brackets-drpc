e83b1d20568a168497dc952ab304e2c873b69c32
Ben Foster <fosterben150@gmail.com>
2020-02-04 00:26:35 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 define(function (require, exports, module) {
     "use strict";
     var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus");
+        Menus          = brackets.getModule("command/Menus"),
+        AppInit        = brackets.getModule("utils/AppInit"),
+        Docs           = brackets.getModule("document/DocumentManager"),
+        EditorManager  = brackets.getModule("editor/EditorManager");
     function reconnect() {
-        window.alert("This would reconnect back to Discord.");
+        let doc = Docs.getCurrentDocument();
+        window.alert(doc.file.fullPath.split("/").pop());
     }
-    var DISCORD_RECONNECT = "discordrpc.reconnect";
-    CommandManager.register("Reconnect to Discord", DISCORD_RECONNECT, reconnect);
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuItem(DISCORD_RECONNECT, "Ctrl-Shift-R");
+    AppInit.appReady(() => {
+      var DISCORD_RECONNECT = "discordrpc.reconnect";
+      CommandManager.register("Reconnect to Discord", DISCORD_RECONNECT, reconnect);
+      var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+      menu.addMenuItem(DISCORD_RECONNECT, "Ctrl-Shift-R");
+
+      let doc = Docs.getCurrentDocument();
+      if (doc) {
+        window.alert(doc.file.fullPath.split("/").pop());
+      } else {window.alert("Tumbleweed");}
+    });
+    EditorManager.on("activeEditorChange",() => {
+      let doc = Docs.getCurrentDocument();
+      if (doc) {
+        window.alert(doc.file.fullPath.split("/").pop());
+      } else {window.alert("Tumbleweed 2");}
+    });
 });
